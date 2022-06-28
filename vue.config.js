4630f8f48be5701258a79cce4adf54e120b89e20
huchun <1575724298@qq.com>
2022-06-29 01:25:49 +0800

    init project

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: suiyue
+ * @email: 1373842098@qq.com
+ * @Date: 2022-06-29 01:19:27
+ * @LastEditors: sj
+ * @LastEditTime: 2022-06-29 01:21:01
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+lintOnSave: false,
 })
