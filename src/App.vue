42cc08429ff894e7cb6eafe6323ae387d6c2f0dd
huchun <1575724298@qq.com>
2022-06-29 02:52:33 +0800

    完成搜集用户信息练习

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,61 @@
 <template>
   <div>
-     <!-- 	v-model:是实现vuejs 变量和表单标签value属性, 双向绑定的指令-->
-     <!-- input  -->
-        <div>
-      <span>用户名:</span>
-      <input type="text" v-model.number="username" />
+    <!-- 练习一， 搜集用户信息 -->
+    <div>
+      <span>选择所在地区</span>：
+      <input type="radio" value="中国大陆" name="area" v-model="area" />中国大陆
+      <input type="radio" value="中国香港" name="area" v-model="area" />中国香港
     </div>
     <div>
-      <span>密码:</span>
-      <input type="password" v-model.trim="pass" />
+      <span>手机号码</span>：
+      <input type="tel" name="phone" v-model.number="phone" />
     </div>
-    <!-- 下拉菜单 -->
-     <div>
-      <span>来自于: </span>
-      <!-- 下拉菜单要绑定在select上 -->
-      <select v-model="from">
-        <option value="北京市">北京</option>
-        <option value="南京市">南京</option>
-        <option value="天津市">天津</option>
-      </select>
-    </div>
-    <!-- checkbox 复选框 -->
-     <div>
-      <!-- (重要)
-      遇到复选框, v-model的变量值
-      非数组 - 关联的是复选框的checked属性
-      数组   - 关联的是复选框的value属性
-       -->
-      <span>爱好: </span>
-      <input type="checkbox" v-model="hobby" value="抽烟">抽烟
-      <input type="checkbox" v-model="hobby" value="喝酒">喝酒
-      <input type="checkbox" v-model="hobby" value="写代码">写代码
-    </div>
-     <div>
-      <span>爱好: </span>
-      <input type="checkbox" v-model="hobby11" value="抽烟">抽烟
-      <input type="checkbox" v-model="hobby11" value="喝酒">喝酒
-      <input type="checkbox" v-model="hobby11" value="写代码">写代码
-    </div>
-    <!-- radio 单选按钮 -->
     <div>
-      <span>性别: </span>
-      <input type="radio" value="男" name="sex" v-model="gender">男
-      <input type="radio" value="女" name="sex" v-model="gender">女
+      <span>年龄</span>：
+      <input type="number" name="age" v-model.number="age" />
     </div>
-     <div>
-      <span>自我介绍</span>
-      <textarea v-model.lazy="intro"></textarea>
+    <div>
+      <span>密码</span>：
+      <input type="password" name="pass" v-model="pass" />
+    </div>
+    <div>
+      <span>爱好：</span>
+      <input type="checkbox" v-model="hobby" value="看书" />看书
+      <input type="checkbox" v-model="hobby" value="学习" />学习
+      <input type="checkbox" v-model="hobby" value="写代码" />写代码
+      <input type="checkbox" v-model="hobby" value="游戏" />游戏
+    </div>
+    <div><input type="checkbox" v-model="isgrow" /> 记住密码</div>
+    ===============================================
+    <div>
+      我来自：{{ area}}
+      <br />
+      手机号码是：{{phone}}
+      <br />
+      年龄是：{{age}}
+      <br />
+      密码是：{{pass}}
+      <br />
+      爱好是：{{hobby}}
+      <br />
+      是否记住密码：{{ isgrow ? '是' : '否' }}
     </div>
   </div>
-
 </template>
 
 <script>
 export default {
-  data(){
-   return {
-     username: '',
-     pass: '',
-     from:'',
-     hobby:[],
-     hobby11:'',
-     gender: '',
-     intro: '',
-   }
-  }
-
+  data() {
+    return {
+      area: '中国大陆',
+      phone: '',
+      age: '',
+      pass: '',
+      hobby:['写代码'],
+      isgrow:''
+    }
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
