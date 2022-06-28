def9d7664e624279fc889628e044e66721491af6
huchun <1575724298@qq.com>
2022-06-29 02:30:11 +0800

    练习了v-model语法

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,71 @@
-<!--
- * @Descripttion: 
- * @version: 
- * @Author: suiyue
- * @email: 1373842098@qq.com
- * @Date: 2022-06-29 01:19:27
- * @LastEditors: sj
- * @LastEditTime: 2022-06-29 02:03:13
--->
 <template>
   <div>
-    <!-- 1. 插值表达式 {{}} -->
-    <h1> {{ msg }}</h1>
-    <h1> {{obj.name }}{{  obj.age>18? '成年':'未成年'}}</h1>
-    <!--2.  v-baind: 属性名=“变量” 简写  ： 属性名=“变量”-->
-    <a :href="address">百度</a>
-    <br>
-    <img :src="imgUrl">
-    <!--3.  v-on : 事件名=“事件处理”  简写  @事件名=“事件处理” -->
-   <h1>{{count}}</h1>
-   <button @click="count++">+1</button>
-   <button @click="addFn(10)">+10</button>
-    <!-- 4. 事件对象 传值 手动添加 $event -->
-    <br>
-     <a :href="address" @click="addressFn(1,$event)">百度</a>
-    <!-- 5. v-on事件修饰符  .stop  阻止冒泡   .prevent 阻止默认事件  .once 事件函数只处理一次-->
-    <div @click="fatherFn">
-      <span @click.stop="fn1">.stop</span>
-      <br>
-       <a :href="address" @click.prevent>百度</a>
-       <br>
-       <span @click.once="onceFn"> .once</span>
+     <!-- 	v-model:是实现vuejs 变量和表单标签value属性, 双向绑定的指令-->
+     <!-- input  -->
+        <div>
+      <span>用户名:</span>
+      <input type="text" v-model.number="username" />
     </div>
-    <!-- 6. v-on按键修饰符  .enter 回车   .esc 退出 -->
-    <input type="text" @keydown.enter="enterFn">
-    <input type="text" @keydown.esc="escFn">
-    
+    <div>
+      <span>密码:</span>
+      <input type="password" v-model.trim="pass" />
+    </div>
+    <!-- 下拉菜单 -->
+     <div>
+      <span>来自于: </span>
+      <!-- 下拉菜单要绑定在select上 -->
+      <select v-model="from">
+        <option value="北京市">北京</option>
+        <option value="南京市">南京</option>
+        <option value="天津市">天津</option>
+      </select>
+    </div>
+    <!-- checkbox 复选框 -->
+     <div>
+      <!-- (重要)
+      遇到复选框, v-model的变量值
+      非数组 - 关联的是复选框的checked属性
+      数组   - 关联的是复选框的value属性
+       -->
+      <span>爱好: </span>
+      <input type="checkbox" v-model="hobby" value="抽烟">抽烟
+      <input type="checkbox" v-model="hobby" value="喝酒">喝酒
+      <input type="checkbox" v-model="hobby" value="写代码">写代码
+    </div>
+     <div>
+      <span>爱好: </span>
+      <input type="checkbox" v-model="hobby11" value="抽烟">抽烟
+      <input type="checkbox" v-model="hobby11" value="喝酒">喝酒
+      <input type="checkbox" v-model="hobby11" value="写代码">写代码
+    </div>
+    <!-- radio 单选按钮 -->
+    <div>
+      <span>性别: </span>
+      <input type="radio" value="男" name="sex" v-model="gender">男
+      <input type="radio" value="女" name="sex" v-model="gender">女
+    </div>
+     <div>
+      <span>自我介绍</span>
+      <textarea v-model.lazy="intro"></textarea>
+    </div>
   </div>
+
 </template>
 
 <script>
-import imgUrl from './assets/logo.png'
 export default {
- data(){
-  return { 
-    msg: 'Hello',
-    obj:{
-      name: 'haa',
-      age: 18
-    },
-    address: 'http://www.baidu.com',
-    imgUrl:imgUrl,
-    count:0
+  data(){
+   return {
+     username: '',
+     pass: '',
+     from:'',
+     hobby:[],
+     hobby11:'',
+     gender: '',
+     intro: '',
+   }
   }
- },
- methods:{
-  addFn(n){
-    this.count += n
-  },
-  addressFn(n,e){
-    e.preventDefault()
-  },
-  fatherFn(){
-    console.log('father');
-  },
-  fn1(){
-    console.log('span click');
-  },
-  onceFn(){
-    console.log('once');
-  },
-  enterFn(){
-    alert(1)
-  },
-  escFn(){
-    alert(2)
-  }
- }
+
 }
 </script>
 
